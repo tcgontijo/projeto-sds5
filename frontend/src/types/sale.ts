4b09eade3715a8f119e8d61634fcdf4a9c5b8559
Tulio Gontijo <tulio_gontijo@hotmail.com>
2021-11-08 00:37:13 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,28 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+import { Seller } from './seller';
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    size?: number;
+    number: number;
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean;
+}
+
 export type SaleSum = {
     sum: number;
     sellerName: string;
--- frontend/src/utils/format.ts
+++ frontend/src/utils/format.ts
@@ -1,6 +1,14 @@
 /* eslint-disable no-restricted-properties */
 /* eslint-disable import/prefer-default-export */
+import { format } from 'date-fns';
+
 export const round = (value: number, precision: number) => {
   const multiplier = Math.pow(10, precision || 0);
   return Math.round(value * multiplier) / multiplier;
+};
+
+export const formatLocalDate = (date: string, pattern: string) => {
+  const dt = new Date(date);
+  const dtDateOnly = new Date(dt.valueOf() + dt.getTimezoneOffset() * 60 * 1000);
+  return format(dtDateOnly, pattern);
 };
