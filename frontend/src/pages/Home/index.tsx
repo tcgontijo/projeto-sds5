9454a92619fdf632aa6699a7d36a2629c70a3424
Tulio Gontijo <tulio_gontijo@hotmail.com>
2021-11-07 23:25:41 -0300

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,26 @@
-// eslint-disable-next-line import/no-unresolved
+/* eslint-disable import/no-unresolved */
+/* eslint-disable max-len */
+
+import Footer from 'components/Footer';
+import Navbar from 'components/Navbar';
+import { Link } from 'react-router-dom';
 
 const Home = () => (
-  <div>
-    <h1>Página home</h1>
-  </div>
+  <>
+    <Navbar />
+    <div className="container">
+      <div className="jumbotron">
+        <h1 className="display-4">DSVendas</h1>
+        <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+        <hr />
+        <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+        <Link to="/dashboard" className="btn btn-primary btn-lg">
+          Acessar o Dashboard
+        </Link>
+      </div>
+    </div>
+    <Footer />
+  </>
 );
 
 export default Home;
