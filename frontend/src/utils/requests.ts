08de060ed480ca98919c633682df986d0f7b273e
Tulio Gontijo <tulio_gontijo@hotmail.com>
2021-11-08 01:07:21 -0300

    Env var

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,2 +1,2 @@
 /* eslint-disable import/prefer-default-export */
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
